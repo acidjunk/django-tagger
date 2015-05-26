1cb6017c89d1c0dc6bdbea328160c257ae2f5fb9
Rene Dohmen <acidjunk@gmail.com>
2015-05-26 15:32:56 +0200

    Fixed URLs

--- tagger/urls.py
+++ tagger/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     url(r'^tag-cloud$', views.tag_cloud, name='tag-cloud'),
     url(r'^tag-it/(?P<model>\w+)/(?P<model_id>\d+)$', views.tag_it, name='tag-it'),
     url(r'^tag/del/(?P<id>\d+)$', views.tag_del, name='tag-del'),
